f9de57d3d18114170a906c065c9f8eace990c048
tigger <tiggerjeo@gmail.com>
2025-01-23 02:06:10 -0600

    added port for deploy

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ app.post("/api/persons", (request, respones) => {
   respones.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("start");
 });
